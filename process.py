e0df5c1f64c47838fd04ba9adf20c6a36948bf34
Amitava Majumder <71118670+amitava-0304@users.noreply.github.com>
2022-07-22 13:09:26 +0530

    Add files via upload

---
+++ process.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import multiprocessing
+
+def printsec(seconds):
+    from datetime import datetime
+    from time import sleep
+    sleep(seconds)
+    print('wait', seconds, 'seconds, time is', datetime.utcnow())
+    
+if __name__ == '__main__':
+    import random    
+    for n in range(3):
+        seconds = random.random()
+        proc = multiprocessing.Process(target=printsec, args=(seconds,))
+        proc.start()
+
